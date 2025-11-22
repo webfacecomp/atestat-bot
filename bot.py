321a9f8914d510697eae67a455c621bb2ffc9e73
khakimov1807 <siu1807siu@gmail.com>
2025-11-22 19:07:54 +0500

    fix token env

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import telebot
 from telebot import types
 
-TOKEN = os.getenv("8514788206:AAGeVapTQe1oGcLt1io5J3zbc4885eh1dZM")
+TOKEN = os.environ.get("BOT_TOKEN") 
 print("TOKEN VALUE >>>", repr(TOKEN))
 bot = telebot.TeleBot(TOKEN)
 
